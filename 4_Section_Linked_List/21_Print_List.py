d3b5eabd01233c31131f8da53f4c3054c45aa6f7
sasha2804 <o.korotushko@gmail.com>
2023-10-21 21:52:35 +0200

    21102023

--- 4_Section_Linked_List/21_Print_List.py
+++ 4_Section_Linked_List/21_Print_List.py
@@ -15,7 +15,7 @@ class LinkedList:
         self.tail = new_node
         self.length = 1
     
-    def append_element(self, value): #adds item to the end of the list
+    def append(self, value): #adds item to the end of the list
         new_node = Node(value)
         if self.head is None:
              self.head = new_node
@@ -138,37 +138,53 @@ class LinkedList:
             temp = temp.next
         print('numver of elements in LL:', self.length)
 
-my_linked_list = LinkedList(4)
-my_linked_list.append_element(5)
-my_linked_list.append_element(6)
-my_linked_list.append_element(7)
-my_linked_list.append_element(8)
-my_linked_list.append_element(9)
 
 
 
+# my_linked_list = LinkedList(4)
+# my_linked_list.append_element(5)
+# my_linked_list.append_element(6)
+# my_linked_list.append_element(7)
+# my_linked_list.append_element(8)
+# my_linked_list.append_element(9)
 # my_linked_list.pop()
 # my_linked_list.pop()
 # my_linked_list.print_list()
 # my_linked_list.prepend(125)
 # my_linked_list.pop_first()
 # my_linked_list.set_value(2,15)
-
-my_linked_list.print_list()
-
+# my_linked_list.print_list()
 # my_linked_list.set_value(2,120)
 # my_linked_list.insert(0,180)
 # my_linked_list.get(3)
-
-
-
 # my_linked_list.remove(3)
-
+# my_linked_list.print_list()
+# my_linked_list.reverse()
 # my_linked_list.print_list()
 
-my_linked_list.reverse()
+def check(expect, actual, message):
+    print(message)
+    print("EXPECTED:", expect)
+    print("RETURNED:", actual)
+    print("PASS" if expect == actual else "FAIL", "\n")
 
-my_linked_list.print_list()
 
-
+linked_list = LinkedList(1)
+linked_list.append(2)
+linked_list.print_list()
+popped_node = linked_list.pop()
+check(2, popped_node.value, "Value of popped node (first pop):")
+check(1, linked_list.head.value, "Head of linked list (after first pop):")
+check(1, linked_list.tail.value, "Tail of linked list (after first pop):")
+check(1, linked_list.length, "Length of linked list (after first pop):")
+popped_node = linked_list.pop()
+check(1, popped_node.value, "Value of popped node (second pop):")
+check(None, linked_list.head, "Head of linked list (after second pop):")
+check(None, linked_list.tail, "Tail of linked list (after second pop):")
+check(0, linked_list.length, "Length of linked list (after second pop):")
+popped_node = linked_list.pop()
+check(None, popped_node, "Popped node from empty linked list (third pop):")
+check(None, linked_list.head, "Head of linked list (after third pop):")
+check(None, linked_list.tail, "Tail of linked list (after third pop):")
+check(0, linked_list.length, "Length of linked list (after third pop):")
 
