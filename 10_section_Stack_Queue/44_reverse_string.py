249dd454a769bd8bcb17e15e4d3c5864d432532b
sasha2804 <O.Korotushko@gmail.com>
2023-11-17 08:21:42 +0100

    123

---
+++ 10_section_Stack_Queue/44_reverse_string.py
@@ -0,0 +1,68 @@
+import re
+
+
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def print_stack(self):
+        for i in range(len(self.stack_list)-1, -1, -1):
+            print(self.stack_list[i])
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+
+    def peek(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list[-1]
+
+    def size(self):
+        return len(self.stack_list)
+
+    def push(self, value):
+        self.stack_list.append(value)
+
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            return self.stack_list.pop()
+
+def reverse_string(string):
+    stack = Stack()
+    rev = ""
+    for char in string:
+        stack.push(char)
+    while not stack.is_empty():
+        rev += stack.pop()
+    return rev
+        
+    
+
+
+
+## WRITE REVERSE_STRING FUNCTION HERE ###
+#                                       #
+#  This is a separate function that is  #
+#  not a method within the Stack class. #
+#  Indent all the way to the left.      #
+#                                       #
+#########################################
+
+
+
+
+my_string = ')('
+
+print ( reverse_string(my_string) )
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    olleh
+
+"""
