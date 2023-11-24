5cec0c59d0ee4ad9447224245709ba37bd3948fd
sasha2804 <O.Korotushko@gmail.com>
2023-11-24 08:24:37 +0100

    24.11.2023

--- 13_section_Trees/49_BST_Instert.py
+++ 13_section_Trees/49_BST_Instert.py
@@ -1,3 +1,5 @@
+
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -11,10 +13,30 @@ class BinarySearchTree:
     
     def insert(self, value):
         new_node = Node(value)
-        if self.root == None:
+        if self.root is None:
             self.root = new_node
             return True
-            
+        temp = self.root
+
+
+
+        while True:
+            if new_node.value == temp.value:
+                return False            
+            if new_node.value < temp.value:
+                if temp.left is None:
+                    temp.left = new_node
+                    return True
+                temp = temp.left
+            else:
+                if temp.right is None:
+                    temp.right = new_node
+                    return True
+                temp = temp.right
+
+        
+
+
 
     ## WRITE INSERT METHOD HERE ##
     #                            #
